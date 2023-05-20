ce06197e285c38d52353ab64a857b7eeaf1fe630
k1m1jong <won72881@naver.com>
2023-05-20 14:40:40 +0900

    db모델 추가

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'port.apps.PortConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
