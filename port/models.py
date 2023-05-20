741aabf5e6bea439155dbeee594491b0aba3d901
k1m1jong <96505642+won7288@users.noreply.github.com>
2023-05-20 14:42:58 +0900

    Wonjong (#3)
    * .
    
    * db모델 추가

--- port/models.py
+++ port/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class myprofile(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+
+class Portlist(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
