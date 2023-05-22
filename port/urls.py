15dc9da68fdc86a8004acab6fd046e0e964c41dd
k1m1jong <won72881@naver.com>
2023-05-22 16:42:20 +0900

    상세보기 URL 매핑..

--- port/urls.py
+++ port/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'port'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('MyPort_details/<int:MyPort_details_id>/', views.MyPort_details, name='MyPort_details'), #상세보기 URL매핑
 ]
