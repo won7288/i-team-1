30e0278cad52a91018b84864189294d3b3354630
k1m1jong <96505642+won7288@users.noreply.github.com>
2023-05-22 16:45:03 +0900

    상세보기 매핑 (#6)
    * .
    
    * db모델 추가
    
    * 기본요소
    
    * 기본 요소
    
    * static, template
    
    * .
    
    * 상세보기 URL 매핑..

--- port/views.py
+++ port/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.http import HttpResponseNotAllowed
+
 
 
 def index(request):
