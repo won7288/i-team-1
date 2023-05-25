d226aad16aa0f86cc77e9bd7bddc9c0bf6832f51
k1m1jong <96505642+won7288@users.noreply.github.com>
2023-05-25 22:29:18 +0900

    Delete port directory

--- port/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from django.http import HttpResponseNotAllowed
-
-
-
-def index(request):
-    return HttpResponse("안녕하세요 portfolio site에 오신것을 환영합니다.")
