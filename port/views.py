c6694e81289177837b9eda6a9a1099becd71645e
k1m1jong <won72881@naver.com>
2023-06-08 23:23:06 +0900

    이름변경

--- port/views.py
+++ port/views.py
@@ -7,10 +7,10 @@ from .models import user_detail
 def index(request):
     return render(request, 'port/main.html')
 
-def my_detail(request):
+def my_Detail(request): #함수이름과 모델이름 구분해서 짓기
     detail = my_detail.objects.first()  
     return render(request, 'port/my_detail.html', {'my_detail': detail})
 
-def user_detail(request):
+def user_Detail(request): #함수이름과 모델이름 구분해서 짓기
     detail = user_detail.objects.first()
     return render(request, 'port/user_detail.html', {'user_detail': detail})
