81cb26cc40c3ed8d8bf913bf35d884ef7243f7c9
k1m1jong <won72881@naver.com>
2023-06-19 16:54:07 +0900

    로그인, 회원가입구현, 생성하기 누르면 오류남, main.html -> profile.html로 이름 변경

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('port/', include('port.urls')),
+    path('common/', include('common.urls')),
 ]
