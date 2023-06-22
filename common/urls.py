888aedf9ded830e1d33b691f4bcdd1acfd697b68
k1m1jong <won72881@naver.com>
2023-06-22 20:47:16 +0900

    로그인/로그아웃 구현

--- common/urls.py
+++ common/urls.py
@@ -6,5 +6,6 @@ app_name = 'common'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,8 +1,21 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from common.forms import UserForm
+from django.urls import reverse
 
-
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('port:profile'))
+        else:
+            return render(request, 'common/login.html', {'error_message': '로그인에 실패했습니다.'})
+    else:
+        return render(request, 'common/login.html')
+    
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -10,9 +23,9 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
-            return redirect('index')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(reverse('port:index'))
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
