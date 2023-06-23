48d76029ee5e99d369afda57e3833bbbc02bedfd
k1m1jong <won72881@naver.com>
2023-06-23 20:43:27 +0900

    프로필페이지 구현

--- port/models.py
+++ port/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
 from django.core.validators import MinValueValidator, MaxValueValidator
+
 class my_detail(models.Model):      #본인 이력 상세페이지
     project_title = models.CharField(max_length=20)
     project_image = models.ImageField()
     content = models.TextField()
     create_date = models.DateTimeField()
 
-class user_detail(models.Model):    #다른유저 이력 상세페이지
-    project_title = models.CharField(max_length=20)
-    project_image = models.ImageField()
-    content = models.TextField()
-    create_date = models.DateTimeField()
-    write_question = models.CharField(max_length=50)
+
 
 class Portfolio(models.Model):
     image = models.ImageField(upload_to='images/')
--- port/views.py
+++ port/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, redirect
 
 from .forms import PortfolioForm, ProfileForm
-#from django.contrib.auth.decorators import login_required #로그인 데코레이터
+from django.contrib.auth.decorators import login_required #로그인 데코레이터
 # Create your views here.
 from .models import my_detail, Portfolio, Profile
-from .models import user_detail
 
 def index(request):
     return render(request, 'common/login.html')
 
+@login_required
 def profile(request):
     return render(request, 'port/profile.html')
 
+@login_required
 def my_Detail(request): #함수이름과 모델이름 구분해서 짓기
     detail = my_detail.objects.first()  
     return render(request, 'port/my_detail.html', {'my_detail': detail})
 
-def user_Detail(request): #함수이름과 모델이름 구분해서 짓기
-    detail = user_detail.objects.first()
-    return render(request, 'port/user_detail.html', {'user_detail': detail})
-
+@login_required
 def port_view(request):
     if request.method == 'POST':
         form = PortfolioForm(request.POST, request.FILES)
@@ -38,14 +36,14 @@ def port_view(request):
 
     return render(request, 'port/portfolio_list.html', context)
 
-
+@login_required
 def profile_view(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
             Profile.objects.all().delete()  # Delete all data in the database
             form.save()  # Save the form data to the database
-            return redirect('port:profile_view')  # Redirect to the same page to display the updated contents
+            return redirect('port:profile')  # Redirect to the same page to display the updated contents
     else:
         form = ProfileForm()
 
